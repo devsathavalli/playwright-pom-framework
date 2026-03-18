0c167b48879f7e74c3339909989dc599aab4b4d3
devsathavalli <dev.sathavalli@gmail.com>
2026-03-18 12:28:01 +0100

    Update login.spec.ts

--- tests/login/login.spec.ts
+++ tests/login/login.spec.ts
@@ -1,1 +1,12 @@
 
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../../pages/login.page';
+
+test('User can login', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+
+  await loginPage.navigate('/');
+  await loginPage.login('demo','demo');
+
+  await expect(page).toHaveURL(/dashboard/);
+});
