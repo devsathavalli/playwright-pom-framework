853eac22756ff3f2a47ebcec4bb3deb58d74fbed
devsathavalli <dev.sathavalli@gmail.com>
2026-03-18 12:28:14 +0100

    Update test.fixture.ts

--- fixtures/test.fixture.ts
+++ fixtures/test.fixture.ts
@@ -1,1 +1,8 @@
+import { test as base } from '@playwright/test';
+import { LoginPage } from '../pages/login.page';
 
+export const test = base.extend({
+  loginPage: async ({ page }, use) => {
+    await use(new LoginPage(page));
+  },
+});
